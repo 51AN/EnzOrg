6e936acdc8e4fd286f79cb8a66c4bb805880c8d8
Nazmul Hossain <nazmulhossain@iut-dhaka.edu>
2022-12-29 02:43:40 +0600

    implemented task settings

--- settingstask/index.php
+++ settingstask/index.php
@@ -1,116 +1,123 @@
 <?php
-   $conn = new mysqli('localhost','root','','spl');
+$conn = new mysqli('localhost', 'root', '', 'spl');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <title>Settings</title>
-    <link rel="shortcut icon" href="/assets/favicon.ico">
-    <link rel="stylesheet" href="./styles.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
-<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <title>Settings</title>
+  <link rel="shortcut icon" href="/assets/favicon.ico">
+  <link rel="stylesheet" href="./styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet">
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <?php
-   session_start();
-   $user_id = $_SESSION['user_id'];
-   if(isset($_POST['update_profile']))
-   {
-      $username = $_POST['update_name'];
-      $email = $_POST['update_email'];
+session_start();
+$taskID =  $_SESSION['taskID'];
+$user_id = $_SESSION['user_id'];
+$name = $_SESSION['taskName'];
+$projectID =  $_SESSION['projectID'];
 
-      mysqli_query($conn, "UPDATE `users` SET username = '$username', email = '$email' WHERE id = '$user_id'") or die('query failed');
-      
-      $old_pass = $_POST['old_pass'];
-      $update_pass = md5($_POST['update_pass']);
-      $temp_pass = $_POST['new_pass'];
-      $new_pass = md5($_POST['new_pass']);
-      $confirm_pass = md5($_POST['confirm_pass']);
-   
-      if(!empty($update_pass) || !empty($new_pass) || !empty($confirm_pass)){
-         if($update_pass != $old_pass){
-            $message[] = 'old password not matched!';
-         }elseif($new_pass != $confirm_pass){
-            $message[] = 'confirm password not matched!';
-         }else if(strlen($temp_pass) < 8 || ctype_upper($temp_pass) || ctype_lower($temp_pass)){
-            $message[] = "Password must be atleast 8 character long and contain uppercase and lowercase";
-         }else{
-            mysqli_query($conn, "UPDATE `users` SET password = '$confirm_pass' WHERE id = '$user_id'") or die('query failed');
-            $message[] = 'password updated successfully!';
-         }
-      }
-
-      $image = $_FILES['update_image']['name'];
-      $imageSize = $_FILES['update_image']['size'];
-      $imageTempName = $_FILES['update_image']['tmp_name'];
-      $imageFolder = 'upload/'. $image;
-   
-      if(!empty($image))
+if (isset($_POST['update_task_button'])) {
+  $taskName = htmlspecialchars($_POST['update_task_name']);
+  $taskDes = htmlspecialchars($_POST['update_task_description']);
+  $due = $_POST['update_due'];
+      if($taskName)
       {
-         if($imageSize > 2000000)
-            $message[] = "Image is too large";
-         else
-         {
-            $imageUpdateQuery = mysqli_query($conn, "UPDATE users SET image = '$image' WHERE id = '$user_id'") or die("query failed");
-         
-            if($imageUpdateQuery)
-            {
-               move_uploaded_file($imageTempName, $imageFolder);
-            }
-            $message[] = "Image updated successfully";   
-         }
+        if($taskName != $name)
+        {
+          $fetchTask = mysqli_query($conn, "SELECT * FROM tasks WHERE taskName = '$taskName' AND projID = $projectID");
+          if(mysqli_num_rows($fetchTask) > 0)
+          {
+            $errmsg = "This task already exists in this project.";
+          }
+          else
+          {
+            $updateTaskName = mysqli_query($conn, "UPDATE `tasks` SET `taskName`='$taskName' WHERE taskID = $taskID");
+            $_SESSION['taskName'] = $taskName;
+            header('Location: '.$_SERVER['PHP_SELF'].'?success');
+          }
+        }
       }
-   }
+      if(!$errmsg){
+        if($taskDes)
+        {
+          $updateTaskDes = mysqli_query($conn, "UPDATE `tasks` SET `taskDes`='$taskDes' WHERE taskID = $taskID");
+          header('Location: '.$_SERVER['PHP_SELF'].'?success');
+        }
+
+        if(isset($_POST['priority']))
+        {
+          $priority = $_POST['priority'];
+          $updateTaskPriority = mysqli_query($conn, "UPDATE `tasks` SET `priority`='$priority' WHERE taskID = $taskID");
+          header('Location: '.$_SERVER['PHP_SELF'].'?success');
+        }
+
+        if(isset($_POST['status']))
+        {
+          $status = $_POST['status'];
+          $updateTaskStatus = mysqli_query($conn, "UPDATE `tasks` SET `status`='$status' WHERE taskID = $taskID");
+          header('Location: '.$_SERVER['PHP_SELF'].'?success');
+        }
+
+        if($due)
+        {
+          $updateDate = mysqli_query($conn, "UPDATE `tasks` SET `due`='$due' WHERE taskID = $taskID");
+          header('Location: '.$_SERVER['PHP_SELF'].'?success');
+        }
+      }
+}
 ?>
 
 <body>
-<?php
-    $message = '';
+  <?php
+  $message = '';
 
-    if(isset($_SESSION['username']))
-        $message = $_SESSION['username'];
-?>
-<section class="container">
-   
+  if (isset($_SESSION['username']))
+    $message = $_SESSION['username'];
+  ?>
+  <section class="container">
+
     <!-- side nav bar begin here -->
     <div class="sidebar">
-    <div class="logo-details">
-      <!-- <i class='bx bxl-c-plus-plus icon'></i> -->
+      <div class="logo-details">
+        <!-- <i class='bx bxl-c-plus-plus icon'></i> -->
         <div class="logo_name">EnzOrg</div>
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list">
-      <!-- <li>
+        <i class='bx bx-menu' id="btn"></i>
+      </div>
+      <ul class="nav-list">
+        <!-- <li>
           <i class='bx bx-search' ></i>
          <input type="text" placeholder="Search...">
          <span class="tooltip">Search</span>
       </li> -->
-      <li>
-        <a href="../dashboard/index.php">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-         <span class="tooltip">Dashboard</span>
-      </li>
-      <li>
-       <a href="../profile/index.php">
-         <i class='bx bx-user' ></i>
-         <span class="links_name">My Profile</span>
-       </a>
-       <span class="tooltip">My Profile</span>
-     </li>
-     <li>
-       <a href="../mytasks/index.php">
-         <i class='bx bx-task' ></i>
-         <span class="links_name">My Tasks</span>
-       </a>
-       <span class="tooltip">My Tasks</span>
-     </li>
-     <!-- <li>
+        <li>
+          <a href="../dashboard/index.php">
+            <i class='bx bx-grid-alt'></i>
+            <span class="links_name">Dashboard</span>
+          </a>
+          <span class="tooltip">Dashboard</span>
+        </li>
+        <li>
+          <a href="../profile/index.php">
+            <i class='bx bx-user'></i>
+            <span class="links_name">My Profile</span>
+          </a>
+          <span class="tooltip">My Profile</span>
+        </li>
+        <li>
+          <a href="../mytasks/index.php">
+            <i class='bx bx-task'></i>
+            <span class="links_name">My Tasks</span>
+          </a>
+          <span class="tooltip">My Tasks</span>
+        </li>
+        <!-- <li>
        <a href="#">
          <i class='bx bx-pie-chart-alt-2' ></i>
          <span class="links_name">Analytics</span>
@@ -131,48 +138,48 @@
        </a>
        <span class="tooltip">Order</span>
      </li> -->
-     <li>
-       <a href="../helpandsupport/index.php">
-         <i class='bx bx-support'></i>
-         <span class="links_name">Help & Support</span>
-       </a>
-       <span class="tooltip">Help & Support</span>
-     </li>
-     <li>
-       <a href="../contact/index.php">
-         <i class='bx bxs-contact'></i>
-         <span class="links_name">Contact</span>
-       </a>
-       <span class="tooltip">Contact</span>
-     </li>
-     
-    </ul>
-  </div>
-<!-- add section here-->
-  <script>
-  let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
+        <li>
+          <a href="../helpandsupport/index.php">
+            <i class='bx bx-support'></i>
+            <span class="links_name">Help & Support</span>
+          </a>
+          <span class="tooltip">Help & Support</span>
+        </li>
+        <li>
+          <a href="../contact/index.php">
+            <i class='bx bxs-contact'></i>
+            <span class="links_name">Contact</span>
+          </a>
+          <span class="tooltip">Contact</span>
+        </li>
 
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    menuBtnChange();
-  });
+      </ul>
+    </div>
+    <!-- add section here-->
+    <script>
+      let sidebar = document.querySelector(".sidebar");
+      let closeBtn = document.querySelector("#btn");
+      let searchBtn = document.querySelector(".bx-search");
 
-  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); 
-  });
+      closeBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+      });
+
+      searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+      });
 
 
-  function menuBtnChange() {
-   if(sidebar.classList.contains("open")){
-     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
-   }else {
-     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
-   }
-  }
-  </script>
+      function menuBtnChange() {
+        if (sidebar.classList.contains("open")) {
+          closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+        } else {
+          closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+        }
+      }
+    </script>
 
 
 
@@ -180,64 +187,69 @@
     <!--  nav bar begins here -->
     <nav>
       <!-- <div class="project__name"><b><?php echo $_SESSION['projectName'] ?></b></div> -->
-        <div class="nav-links">
-            <ul>
-                <!-- elements of nav bar  -->
-                <li><a href="../Homepage/index.php">HOME</a></li>
-                <li><a href="../login/index.php">LOG OUT</a></li>
-                <!-- Write profile name here -->
-                <?php echo '<li><a href="../profile/index.php">'.$message.'</a></li>';?>
-                <li><a href="../about/index.php">ABOUT</a></li>
-                <li><a href="../contact/index.php">CONTACT</a></li>
-            </ul>
-        </div>
+      <div class="nav-links">
+        <ul>
+          <!-- elements of nav bar  -->
+          <li><a href="../Homepage/index.php">HOME</a></li>
+          <li><a href="../login/index.php">LOG OUT</a></li>
+          <!-- Write profile name here -->
+          <?php echo '<li><a href="../profile/index.php">' . $message . '</a></li>'; ?>
+          <li><a href="../about/index.php">ABOUT</a></li>
+          <li><a href="../contact/index.php">CONTACT</a></li>
+        </ul>
+      </div>
     </nav>
 
-<div class="update-profile">
-<!-- php code user information -->
+    <div class="update-profile">
+      <!-- php code user information -->
 
-   <?php
+      <?php
 
-      $select = mysqli_query($conn, "SELECT * FROM USERS WHERE id = '$user_id'") or die('query failed');
-      if(mysqli_num_rows($select) > 0)
-      {
-         $fetch = mysqli_fetch_assoc($select);
+      $select = mysqli_query($conn, "SELECT * FROM tasks WHERE taskID = '$taskID'") or die('query failed');
+      if (mysqli_num_rows($select) > 0) {
+        $fetch = mysqli_fetch_assoc($select);
       }
-   ?>
+      ?>
 
-   <form action="" method="POST" enctype="multipart/form-data">
-      
-
-      
-      <div class="flex">
-         <div class="inputBox">
+      <form action="" method="POST" enctype="multipart/form-data">
+        <div class="flex">
+          <div class="inputBox">
             <span>Update Task Name </span>
             <!-- put values here from database -->
-            <input type="text" name="update_name" value="<?php echo $fetch['username'];?>" class="box">
+            <input type="text" name="update_task_name" value="<?php echo $fetch['taskName']; ?>" class="box">
             <span>Update Task Description </span>
-            <input type="text" name="update_email" value="<?php echo $fetch['email'];?>" class="box">
+            <input type="text" name="update_task_description" value="<?php echo $fetch['taskDes']; ?>" class="box">
             <span>Update Priority :</span>
-            <input type="text" name="update_image" value="" class="box">
-         </div>
-         <div class="inputBox">
-            <input type="hidden" name="old_pass" value="<?php echo $fetch['password']; ?>">
+            <select class="box" id="priority" name="priority">
+              <option disabled selected hidden><?php echo $fetch['priority']; ?></option>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </select>
+          </div>
+          <div class="inputBox">
             <span>Update Status </span>
-            <input type="password" name="update_pass" class="box">
+            <select class="box" id="status" name="status">
+              <option disabled selected hidden><?php echo $fetch['status']; ?></option>
+              <option>Completed</option>
+              <option>In Progress</option>
+              <option>Future</option>
+            </select>
             <span>Update Due Date </span>
-            <input type="password" name="new_pass"  class="box">
-            
-         </div>
-      </div>
-      <div class="buttons">
-         <input type="submit" value="Update Project" name="update_profile" class="btn">
-      </div>
-      <div class="buttons">
-      <a href="../projectpageadmin/index.php"><input type="button" value="Go Back" name="go_back" class="btn"></a>
-      </div>
-   </form>
+            <input type="date" name="update_due" class="box" value="<?php echo $fetch['due']; ?>">
+          </div>
+        </div>
+        <div class="buttons">
+          <input type="submit" value="Update Task" name="update_task_button" class="btn">
+        </div>
+        <div class="buttons">
+          <a href="../taskpageadmin/index.php"><input type="button" value="Go Back" name="go_back" class="btn"></a>
+        </div>
+      </form>
 
-</div>
-</section>
+    </div>
+  </section>
 
 </body>
+
 </html>
