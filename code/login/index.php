9b6e7f4b19026fefcbf9824af78e48ee656b92c4
Dihan141 <rhidwanrashid@iut-dhaka.edu>
2023-01-02 11:20:02 +0600

    connecting to database from one file

--- code/login/index.php
+++ code/login/index.php
@@ -18,7 +18,7 @@
     if(isset($_POST['submit'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $conn = new mysqli('localhost','root','','spl');
+        include "../dbconnect.php";
 
         if($conn -> connect_error){
             die('Connection Failed : ' .$conn->connect_error);
